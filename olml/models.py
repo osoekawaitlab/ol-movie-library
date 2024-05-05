ab72f3be4dd6dedbe9cf3d091d01209a38530285
osoken <osoken@osoekawalab.com>
2024-05-05 21:50:36 +0900

    feat(models): add some models

---
+++ olml/models.py
@@ -0,0 +1,44 @@
+from typing import Annotated, Literal
+
+from oltl import BaseEntity, BaseUpdateTimeAwareModel
+from pydantic import Field, FilePath
+
+from .types import (
+    FrameId,
+    FrameNumber,
+    MetadataId,
+    MovieId,
+    SegmentId,
+    SourceId,
+    SourceType,
+)
+
+
+class Metadata(BaseEntity[MetadataId], BaseUpdateTimeAwareModel):  # type: ignore[misc]
+    pass
+
+
+class BaseMovieSource(BaseEntity[SourceId], BaseUpdateTimeAwareModel):  # type: ignore[misc]
+    type: SourceType
+
+
+class LocalFileSource(BaseMovieSource):
+    type: Literal[SourceType.LOCAL_FILE] = SourceType.LOCAL_FILE
+    path: FilePath
+
+
+Source = Annotated[LocalFileSource, Field(discriminator="type")]
+
+
+class Movie(BaseEntity[MovieId], BaseUpdateTimeAwareModel):  # type: ignore[misc]
+    source: Source
+
+
+class Segment(BaseEntity[SegmentId], BaseUpdateTimeAwareModel):  # type: ignore[misc]
+    movie: MovieId
+    start_frame: FrameId
+    end_frame: FrameId
+
+
+class Frame(BaseEntity[FrameId], BaseUpdateTimeAwareModel):  # type: ignore[misc]
+    number: FrameNumber
--- olml/types.py
+++ olml/types.py
@@ -1,4 +1,6 @@
-from oltl import Id
+from enum import Enum
+
+from oltl import Id, LowerBoundIntegerMixIn
 
 
 class MovieId(Id):
@@ -7,3 +9,25 @@ class MovieId(Id):
 
 class FrameId(Id):
     pass
+
+
+class SegmentId(Id):
+    pass
+
+
+class SourceId(Id):
+    pass
+
+
+class MetadataId(Id):
+    pass
+
+
+class SourceType(str, Enum):
+    LOCAL_FILE = "LOCAL_FILE"
+
+
+class FrameNumber(LowerBoundIntegerMixIn):
+    @classmethod
+    def get_min_value(cls) -> int:
+        return 0
